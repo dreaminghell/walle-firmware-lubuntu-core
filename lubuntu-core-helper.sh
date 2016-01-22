5d1d886ee25bc13daea0ecc259d8c8fa6f828868
Meiyou Chen <cmy@rock-chips.com>
2016-01-22 18:03:55 +0800

    support auto login for walle

--- lubuntu-core-helper.sh
+++ lubuntu-core-helper.sh
@@ -11,3 +11,11 @@ export LC_ALL=C
 apt-get -y install lubuntu-core
 apt-get -y install policykit-1 chromium-browser
 apt-get clean
+
+mkdir -p /etc/lightdm/lightdm.conf.d
+rm -rf /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "[SeatDefaults]" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "autologin-user=walle" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "autologin-user-timeout=0" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "user-session=Lubuntu" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+chmod 0644 /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
