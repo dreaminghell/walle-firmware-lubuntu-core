5a5481f2e8267e40b7750dd24149ac87254d9d3a
Meiyou Chen <cmy@rock-chips.com>
2016-01-22 14:17:47 +0800

    add script for make lubuntu-core

---
+++ lubuntu-core-helper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# setup environment
+mount none -t proc /proc
+mount none -t sysfs /sys
+mount none -t devpts /dev/pts
+export HOME=/root
+export LC_ALL=C
+
+# Install deb
+apt-get -y install lubuntu-core
+apt-get clean
+
