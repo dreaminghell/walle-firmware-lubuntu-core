8724c6ce0e68cd3c1d6997ed72357d2ec22e94a6
Meiyou Chen <cmy@rock-chips.com>
2016-01-28 19:18:29 +0800

    add language pack and select chinese by default

--- lubuntu-core-helper.sh
+++ lubuntu-core-helper.sh
@@ -9,7 +9,10 @@ export LC_ALL=C
 
 # Install deb
 apt-get -y install lubuntu-core
-apt-get -y install policykit-1 chromium-browser
+#apt-get -y install wbritish wamerican language-selector-gnome
+apt-get -y install language-pack-en language-pack-zh-hans
+apt-get -y install fonts-arphic-uming fonts-arphic-ukai fonts-droid
+apt-get -y install policykit-1 chromium-browser chromium-browser-l10n
 apt-get -y install glmark2-es2
 apt-get clean
 
@@ -20,6 +23,10 @@ echo "autologin-user=walle" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
 echo "autologin-user-timeout=0" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
 echo "user-session=Lubuntu" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
 chmod 0644 /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+
+# use Chinese language by default
+echo "zh_CN.UTF-8" >> /etc/environment
+echo "zh_CN:zh:en_US:en" >> /etc/environment
 
 # use libmali for glmark2 with X11
 ln -sf libmali.x11.so /usr/lib/libmali.so
